73bb2aabfb16f5102f71dfc876c13c1808543a51
t106362512 <t106362512@gmail.com>
2025-05-30 19:14:08 -0400

    feat: Add platform-specific compiler arguments for Cython extensions

--- hook-nikke_cjjc_automator.py
+++ hook-nikke_cjjc_automator.py
@@ -2,8 +2,18 @@ from PyInstaller.utils.hooks import collect_submodules
 from distutils.core import setup
 from Cython.Build import cythonize
 from pathlib import Path
+import sys
 
 PACKAGE_NAME = 'src/nikke_cjjc_automator'
+
+extra_compile_args = []
+if sys.platform == 'win32':
+    extra_compile_args = ['/O2']
+else:
+    extra_compile_args = ['-O3', '-march=native']
+
+for ext in extensions:
+    ext.extra_compile_args = extra_compile_args
 
 setup(
     ext_modules=cythonize([str(p) for p in Path(PACKAGE_NAME).rglob('*.py')], compiler_directives={'language_level': "3", 'boundscheck': False, 'wraparound': False, 'cdivision': True, 'initializedcheck': False, 'infer_types': True}),
