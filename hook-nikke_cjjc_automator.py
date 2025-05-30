5c44c889add9599a372f8d361914dd0f854f80a9
t106362512 <t106362512@gmail.com>
2025-05-30 18:32:46 -0400

    feat: Add Cython support and update build scripts for improved packaging

---
+++ hook-nikke_cjjc_automator.py
@@ -0,0 +1,13 @@
+from PyInstaller.utils.hooks import collect_submodules
+from distutils.core import setup
+from Cython.Build import cythonize
+from pathlib import Path
+
+PACKAGE_NAME = 'nikke_cjjc_automator'
+
+setup(
+    ext_modules=cythonize([str(p) for p in Path(PACKAGE_NAME).rglob('*.py')], compiler_directives={'language_level': "3"}),
+    script_args=["build_ext", "--inplace"]
+)
+
+hiddenimports = collect_submodules(PACKAGE_NAME)
