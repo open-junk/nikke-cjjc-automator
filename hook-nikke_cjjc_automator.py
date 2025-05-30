eda54f130658ce8988cb74ed42df2bb8802a331a
t106362512 <t106362512@gmail.com>
2025-05-30 19:09:16 -0400

    feat: Enhance Cython build configuration with additional compiler directives

--- hook-nikke_cjjc_automator.py
+++ hook-nikke_cjjc_automator.py
@@ -6,7 +6,7 @@ from pathlib import Path
 PACKAGE_NAME = 'src/nikke_cjjc_automator'
 
 setup(
-    ext_modules=cythonize([str(p) for p in Path(PACKAGE_NAME).rglob('*.py')], compiler_directives={'language_level': "3"}),
+    ext_modules=cythonize([str(p) for p in Path(PACKAGE_NAME).rglob('*.py')], compiler_directives={'language_level': "3", 'boundscheck': False, 'wraparound': False, 'cdivision': True, 'initializedcheck': False, 'infer_types': True}),
     script_args=["build_ext", "--inplace"]
 )
 
