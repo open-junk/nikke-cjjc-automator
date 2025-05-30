ddf8da8c7b3ee2414c6f3544fc125663db996522
t106362512 <t106362512@gmail.com>
2025-05-30 18:53:44 -0400

    refactor: Update PACKAGE_NAME to include 'src/' for correct module path

--- hook-nikke_cjjc_automator.py
+++ hook-nikke_cjjc_automator.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 from pathlib import Path
 
-PACKAGE_NAME = 'nikke_cjjc_automator'
+PACKAGE_NAME = 'src/nikke_cjjc_automator'
 
 setup(
     ext_modules=cythonize([str(p) for p in Path(PACKAGE_NAME).rglob('*.py')], compiler_directives={'language_level': "3"}),
